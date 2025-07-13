0a7280ab1e83bf97d444abd37015e5f29bf43437
khalidx21 <khalid.lam.21@gmail.com>
2025-07-13 16:18:31 +0100

    Delete of the add-gitkeep.py file

--- add-getkeep.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for root, dirs, files in os.walk("projets"):
-    for dir_name in dirs:
-        if dir_name == "solutions":
-            path = os.path.join(root, dir_name, ".gitkeep")
-            if not os.path.exists(path):
-                open(path, "w").close()
-                print("Added:", path)
